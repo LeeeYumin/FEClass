da3a86f436825f164cd0226c3210f785883f10a4
LeeeYumin <152114081+LeeeYumin@users.noreply.github.com>
2024-08-06 14:39:00 +0900

    day1
    0806_오프라인 첫수업

---
+++ helloDay/src/main/java/com/example/helloDay/HelloDayApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloDay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloDayApplication.class, args);
+	}
+
+}
---
+++ helloDay/src/test/java/com/example/helloDay/HelloDayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloDay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
