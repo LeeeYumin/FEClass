c62c5653321a0b6af9211df1a11657d2c34b19ed
LeeeYumin <152114081+LeeeYumin@users.noreply.github.com>
2024-08-07 16:36:29 +0900

    0807_modal, kakao

---
+++ templeexam/src/test/java/com/example/templeexam/TempleexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.templeexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempleexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
