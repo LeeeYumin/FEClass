448b37d7c21b4666f710415175a89f8554cf4b6b
LeeeYumin <152114081+LeeeYumin@users.noreply.github.com>
2024-08-17 09:51:27 +0900

    0816_aws는 못하고 전까지만

---
+++ visitors/src/main/java/com/example/visitors/VisitorRepository.java
@@ -0,0 +1,8 @@
+package com.example.visitors;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisitorRepository extends JpaRepository<Visitor, Integer> {
+
+	
+}
